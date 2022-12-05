3d9798e9d8845ff289c83dc664e67d69d2aacc07
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-12-05 14:44:46 -0800

    change correct image to portfoilo section

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -15,20 +15,20 @@ function ProjectsList () {
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
-                    <h2>Password Generator</h2>
-                    <a href="https://yingyliu.github.io/password-generator/"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <h2>Work day Scheduler</h2>
+                    <a href="https://yingyliu.github.io/work-day-scheduler/"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
-                    <h2>Password Generator</h2>
-                    <a href="https://yingyliu.github.io/password-generator/"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <h2>Weather Dashboard</h2>
+                    <a href="https://yingyliu.github.io/weather-dashboard/"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
-                    <h2>Password Generator</h2>
-                    <a href="https://yingyliu.github.io/password-generator/"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <h2>newbieBay</h2>
+                    <a href="https://darxmarx.github.io/california-bay-area-guide/"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
             </ul>
@@ -36,47 +36,5 @@ function ProjectsList () {
         
     )
 }
-
-// function ProjectsList () {
-//     return [
-//         {
-//             name: 'Password Generator',
-//             category: 'projects',
-//             description: 'User can use this site genereating random password.',
-//             url: 'https://yingyliu.github.io/password-generator/',
-//             githubURL: 'https://github.com/yingyliu/password-generator'
-//         },
-//         {
-//             name: 'Work day Scheduler',
-//             category: 'projects',
-//             description: 'User can use this site recording their 9am-5pm schedule.',
-//             url: 'https://yingyliu.github.io/work-day-scheduler/',
-//             githubURL: 'https://github.com/yingyliu/work-day-scheduler'
-//         }, 
-//         {
-//             name: 'Weather Dashboard',
-//             category: 'projects',
-//             description: 'User can use this site searching for weather forcast of a specific city.',
-//             url: 'https://yingyliu.github.io/work-day-scheduler/',
-//             githubURL: 'https://github.com/yingyliu/work-day-scheduler'
-//         },
-//         {
-        
-//             name: 'Note Taker',
-//             category: 'projects',
-//             description: 'User can use this site to make their own note.',
-//             url: 'https://note-taker-yingyliu.herokuapp.com/',
-//             githubURL: 'https://github.com/yingyliu/note-taker'
-            
-//         },
-//         {
-//             name: 'newbieBay',
-//             category: 'projects',
-//             description: 'A handy, intuitive website that grants users a number of resources suited for life in California bay area.',
-//             url: 'https://darxmarx.github.io/california-bay-area-guide/',
-//             githubURL: 'https://github.com/Darxmarx/california-bay-area-guide'
-//         }
-//     ]
-// }
 
 export default ProjectsList;
