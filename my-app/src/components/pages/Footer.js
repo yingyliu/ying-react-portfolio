c95d066157f3399725b971c6c488a8baaf0734be
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-12-04 19:53:32 -0800

    add footer section

---
+++ my-app/src/components/pages/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <div className="page-footer" id="footer">
+            <a href="https://github.com/yingyliu"><img src="fa-brands fa-github" alt="github-logo" className="icon"/></a>
+            <a href="https://www.linkedin.com/in/ying-ying-l-5865a4136/"><img src="fa-brands fa-linkedin" alt="linkedin-logo" className="icon"/></a>
+            <a href="https://medium.com/@yingying.liu"><img src="fa-brands fa-medium" alt="medium-logo" className="icon"/></a>
+        </div>
+    )
+}
+
+export default Footer;
