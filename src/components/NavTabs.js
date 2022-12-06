4d4b78a7e3b2219febd08de84958d4738479745e
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-12-05 20:27:03 -0800

    deleted not use file and remove unnecessary syntax

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './styles/NavTabs.css';
 import Nav from 'react-bootstrap/Nav';
 
 function NavTabs({ currentPage, handlePageChange }) {
@@ -18,43 +17,6 @@ function NavTabs({ currentPage, handlePageChange }) {
             <Nav.Link onClick={() =>handlePageChange("Resume")}>Resume</Nav.Link>
         </Nav.Item>
       </Nav>
-
-
-
-        // <ul className="nav nav-tabs">
-        //     <li className="nav-item">
-        //         <a
-        //         href="#about"
-        //         onClick={() => handlePageChange('About')}
-        //         className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
-        //             About
-        //         </a>
-        //     </li>
-        //     <li className="nav-item">
-        //         <a
-        //         href="#portfolio"
-        //         onClick={() => handlePageChange('Portfolio')}
-        //         className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
-        //             Portfolio
-        //         </a>
-        //     </li>
-        //     <li className="nav-item">
-        //         <a
-        //         href="#contact"
-        //         onClick={() => handlePageChange('Contact')}
-        //         className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
-        //             Contact
-        //         </a>
-        //     </li>
-        //     <li className="nav-item">
-        //         <a
-        //         href="#resume"
-        //         onClick={() => handlePageChange('Resume')}
-        //         className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>
-        //             Resume
-        //         </a>
-        //     </li>
-        // </ul>
     );
 }
 
