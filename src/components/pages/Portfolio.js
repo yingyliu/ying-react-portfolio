300018c33389a943f3565b11722c3c5b0b5bcdcb
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-12-15 16:19:45 -0800

    edit portfolio part

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,10 +7,16 @@ function ProjectsList () {
         <section>
             <h1>Portfolio</h1>
             <ul className='projects-images'>
+                 <li>
+                    <div className='d-flex projects-display'>
+                    <h2>Artaholic</h2>
+                    <a href="https://github.com/Dempleon/artaholic" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    </div>
+                </li>
                 <li>
                     <div className='d-flex projects-display'>
-                    <h2>Password Generator</h2>
-                    <a href="https://yingyliu.github.io/password-generator/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <h2>newbieBay</h2>
+                    <a href="https://darxmarx.github.io/california-bay-area-guide/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
                 <li>
@@ -27,12 +33,6 @@ function ProjectsList () {
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
-                    <h2>newbieBay</h2>
-                    <a href="https://darxmarx.github.io/california-bay-area-guide/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
-                    </div>
-                </li>
-                <li>
-                    <div className='d-flex projects-display'>
                     <h2>Code Quiz</h2>
                     <a href="https://yingyliu.github.io/code-quiz/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
@@ -41,12 +41,6 @@ function ProjectsList () {
                     <div className='d-flex projects-display'>
                     <h2>Note Taker</h2>
                     <a href="https://yingyliu.github.io/note-taker/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
-                    </div>
-                </li>
-                <li>
-                    <div className='d-flex projects-display'>
-                    <h2>Artaholic</h2>
-                    <a href="https://github.com/Dempleon/artaholic" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
             </ul>
