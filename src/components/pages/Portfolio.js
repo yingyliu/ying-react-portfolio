cab6cbaa472b3544de25f510a4db3e10745bc493
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2023-03-07 17:03:36 -0800

    add rel

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -11,37 +11,37 @@ function ProjectsList () {
                  <li>
                     <div className='d-flex projects-display'>
                     <h2>Artaholic</h2>
-                    <a href="https://github.com/Dempleon/artaholic" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <a href="https://github.com/Dempleon/artaholic" target="_blank" rel="noreferrer"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
                     <h2>newbieBay</h2>
-                    <a href="https://darxmarx.github.io/california-bay-area-guide/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <a href="https://darxmarx.github.io/california-bay-area-guide/" target="_blank" rel="noreferrer"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
                     <h2>Work day Scheduler</h2>
-                    <a href="https://yingyliu.github.io/work-day-scheduler/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <a href="https://yingyliu.github.io/work-day-scheduler/" target="_blank" rel="noreferrer"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
                     <h2>Weather Dashboard</h2>
-                    <a href="https://yingyliu.github.io/weather-dashboard/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <a href="https://yingyliu.github.io/weather-dashboard/" target="_blank" rel="noreferrer"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
                     <h2>Code Quiz</h2>
-                    <a href="https://yingyliu.github.io/code-quiz/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <a href="https://yingyliu.github.io/code-quiz/" target="_blank" rel="noreferrer"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
                 <li>
                     <div className='d-flex projects-display'>
                     <h2>Note Taker</h2>
-                    <a href="https://yingyliu.github.io/note-taker/" target="_blank"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
+                    <a href="https://yingyliu.github.io/note-taker/" target="_blank" rel="noreferrer"><img src={githubicon} alt="github-logo" className="social-media-icon"/></a>
                     </div>
                 </li>
             </ul>
