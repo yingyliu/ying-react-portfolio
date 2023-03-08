c9411a9800a5ca08030b0fe6a281e82cd1a104ac
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2023-03-07 20:28:52 -0800

    edit navtab

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 
+
+
 function NavTabs({ currentPage, handlePageChange }) {
     return (
         <Nav className="justify-content-end">
